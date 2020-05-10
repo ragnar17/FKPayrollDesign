2fe135cf48b6666454fa448966e47e3eeba2b16a
ragnar17 <edwardamit17@gmail.com>
2020-05-10 13:47:25 +0530

    Added UI Menu 		Add a new Employee 		Show Employees 		Exit

---
+++ SourceCode/ui/menu.java
@@ -0,0 +1,11 @@
+package SourceCode.ui;
+
+public class menu {
+    public static void mainshow()
+    {
+        System.out.println("1] Add a New Employee.");
+        System.out.println("2] Show Employees.");
+        System.out.println("E] Exit.");
+        System.out.println("Choice $: ");
+    }
+}
