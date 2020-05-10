7049ab3b3c38196ace8bcfffbe7afeeda48427e1
ragnar17 <edwardamit17@gmail.com>
2020-05-10 18:37:24 +0530

    Added choice in menu.java

--- SourceCode/ui/menu.java
+++ SourceCode/ui/menu.java
@@ -6,6 +6,6 @@ public class menu {
         System.out.println("1] Add a New Employee.");
         System.out.println("2] Show Employees.");
         System.out.println("E] Exit.");
-        System.out.println("Choice $: ");
+        System.out.print("Choice $: ");
     }
 }
