6c2e3401001f25e46268d17561a11a4443d37674
ragnar17 <edwardamit17@gmail.com>
2020-05-11 03:50:47 +0530

    Added method to create employee object from values retrived from db

--- SourceCode/mapper/employeeMapper.java
+++ SourceCode/mapper/employeeMapper.java
@@ -2,7 +2,10 @@ package SourceCode.mapper;
 
 /* Map the Object Variables to Table Columns */
 
+import SourceCode.entity.Employee;
+
 import java.util.*;
+import java.lang.reflect.*;
 
 public class employeeMapper {
     private static final String[] memberVariableName = new String[]{"id","firstName","lastName","mobileNo",
@@ -11,23 +14,8 @@ public class employeeMapper {
             "email","address","type"};
     private static final String tableName = "employee";
 
-    public static String mappedTo(String key)
-    {
-        String value = "";
-        for(int i=0 ; i<columnName.length ; i++)
-        {
-            if(memberVariableName[i].equals(key))
-            {
-                value = columnName[i];
-                break;
-            }
-        }
-        return value;
-    }
-    public static String[] getField()
-    {
-        return memberVariableName;
-    }
+
+    public static String[] getField() { return memberVariableName; }
     public static String[] getColumn()
     {
         return columnName;
@@ -35,5 +23,28 @@ public class employeeMapper {
     public static String getTableName()
     {
         return tableName;
+    }
+
+    public static Employee getEmployeeMapped(List<String> values) {
+        Employee employee = new Employee();
+        Field[] fields = Employee.class.getDeclaredFields();
+        HashMap<String, String> fieldValue = mapper.getFieldColumn(employeeMapper.getField(), values.toArray(new String[0]));
+        for (Field field : fields) {
+            if (field.getType().getSimpleName().equals("int")) {
+                try {
+                    field.set(employee, Integer.parseInt(fieldValue.get(field.getName())));
+                } catch (Exception ex) {
+                    System.out.println(ex);
+                }
+            } else {
+                try {
+                    field.set(employee, fieldValue.get(field.getName()));
+                } catch (Exception ex) {
+                    System.out.println(ex);
+                }
+            }
+
+        }
+        return employee;
     }
 }
