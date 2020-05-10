3d47355f4da5455559c8f2139a60e9b076263435
ragnar17 <edwardamit17@gmail.com>
2020-05-11 01:47:53 +0530

    Fixed the Select Query Building Method: 		Added conditon option (Where Clause)

--- SourceCode/sql/queryBuilder.java
+++ SourceCode/sql/queryBuilder.java
@@ -1,17 +1,31 @@
 package SourceCode.sql;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class queryBuilder {
-    public static String getDataFromSingle(String tableName, String[] attributes)
+    public static String getDataFromSingle(String tableName, String[] attributes, HashMap<String,String> conditionForSelect)
     {
         String statement = "SELECT ";
         int cnt = 0;
         for(String attribute : attributes){
-            if(cnt==1)
+            if(cnt!=0)
                 statement = statement + ", ";
             statement = statement+ attribute;
             cnt=1;
         }
-        statement = statement +" FROM "+ tableName+";";
+        cnt=0;
+        statement = statement +" FROM "+ tableName;
+        for(Map.Entry mapElement : conditionForSelect.entrySet())
+        {
+            if(cnt!=0)
+                statement = statement + " AND ";
+            else
+                statement = statement + " WHERE ";
+            statement = statement + mapElement.getKey()+"="+mapElement.getValue();
+            cnt=1;
+        }
+        statement = statement + ";";
         return statement;
 
     }
