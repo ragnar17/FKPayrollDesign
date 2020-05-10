57be96e49f55f8667b24d8ac8594b1f6c8334036
ragnar17 <edwardamit17@gmail.com>
2020-05-10 13:21:36 +0530

    Added QueryExecutor 		Methods: 			getData(cmd,attributes count) Implemented 			addData(cmd) Implemented

---
+++ SourceCode/dbms/queryExecutor.java
@@ -0,0 +1,45 @@
+package SourceCode.dbms;
+
+import SourceCode.config.mysql;
+import java.sql.*;
+import java.util.*;
+
+
+public class queryExecutor {
+    public static List<List<String> > getData(String cmd,int attributeCount) {
+        List<List<String> > entries = new ArrayList<>();
+        try(Connection con = DriverManager.getConnection(mysql.getUrl(),mysql.getUser(),mysql.getPassword())) {
+//            System.out.println("Connection Established");
+            Statement statement = con.createStatement();
+            ResultSet resultSet = statement.executeQuery(cmd);
+
+            while(resultSet.next())
+            {
+                List<String> temp = new ArrayList<>();
+                for(int i=1 ; i <= attributeCount ; i++) {
+                    temp.add(resultSet.getString(i));
+                }
+                entries.add(temp);
+            }
+        }
+        catch (SQLException throwable) {
+            throwable.printStackTrace();
+        }
+        return entries;
+
+    }
+
+    public static void addData(String cmd)
+    {
+        try(Connection con = DriverManager.getConnection(mysql.getUrl(),mysql.getUser(),mysql.getPassword())) {
+
+            Statement statement = con.createStatement();
+            statement.executeUpdate(cmd);
+//            System.out.println(cmd);
+        }
+        catch (SQLException throwable) {
+            throwable.printStackTrace();
+        }
+    }
+    
+}
