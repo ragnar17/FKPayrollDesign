b2baa6a156255a4c09ce7130b1e1307dbe899e4b
ragnar17 <edwardamit17@gmail.com>
2020-05-11 00:23:58 +0530

    Added UI. 	Main Driving Application

---
+++ SourceCode/payRoll.java
@@ -0,0 +1,35 @@
+package SourceCode;
+
+import SourceCode.feature.insert;
+import SourceCode.ui.menu;
+import SourceCode.ui.reader;
+
+public class payRoll {
+
+    public static void main(String[] args)
+    {
+        boolean check = true;
+        String choice="0";
+        while(check)
+        {
+            menu.mainshow();
+            choice = reader.get();
+            switch(choice){
+                case "1":
+                    insert.insertEmployee();
+                    break;
+                case "2":
+                    System.out.println("Show Employee");
+                    //Show Employee Method;
+                    break;
+                case "3":
+                    System.out.println("Exiting....... Bye! ");
+                    check = false;
+                    break;
+                default:
+                    System.out.println("We are in the process ,don't be over smart give a valid Input.");
+                    break;
+            }
+        }
+    }
+}
